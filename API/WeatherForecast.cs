4fa354b71f93e42caf74df9ad9a53d4b9225c515
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-02-19 21:19:50 +0100

    Remove https settings

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace API;
 public class WeatherForecast
 {
     public DateOnly Date { get; set; }
-    // Test git
+    
     public int TemperatureC { get; set; }
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
