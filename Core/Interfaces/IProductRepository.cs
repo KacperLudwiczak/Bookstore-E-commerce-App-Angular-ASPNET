e7c432b32819ee98743d7a9ad6a88a71da195091
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-02-21 13:51:09 +0100

    Add two methods

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -6,8 +6,8 @@ public interface IProductRepository
 {
     Task<IReadOnlyList<Product>> GetProductsAsync();
     Task<Product?> GetProductByIdAsync(int id);
-    // Task<IReadOnlyList<string>> GetGenreAsync();
-    // Task<IReadOnlyList<string>> GetPublisherAsync();
+    Task<IReadOnlyList<string>> GetGenreAsync();
+    Task<IReadOnlyList<string>> GetPublisherAsync();
     void AddProduct(Product product);
     void UpdateProduct(Product product);
     void DeleteProduct(Product product);
