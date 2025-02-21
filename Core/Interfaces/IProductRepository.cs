b7c08d3679c34911300e187c7907fd1d5bea70f9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-02-21 13:09:32 +0100

    Comment IProductRepository code

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -4,10 +4,10 @@ namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
+    Task<IReadOnlyList<Product>> GetProductsAsync();
     Task<Product?> GetProductByIdAsync(int id);
-    Task<IReadOnlyList<string>> GetBrandsAsync();
-    Task<IReadOnlyList<string>> GetTypesAsync();
+    // Task<IReadOnlyList<string>> GetGenreAsync();
+    // Task<IReadOnlyList<string>> GetPublisherAsync();
     void AddProduct(Product product);
     void UpdateProduct(Product product);
     void DeleteProduct(Product product);
