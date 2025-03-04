b736be2e63d9b3926754912dca11ed729891ebd7
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-04 10:12:24 +0100

    Add GenericRepository service

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<StoreContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
