b72e8038da6f4c367c8e452adaad5c3993011ed9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-06 14:49:43 +0100

    Change variables

--- API/DTOs/CreateProductDto.cs
+++ API/DTOs/CreateProductDto.cs
@@ -17,10 +17,10 @@ public class CreateProductDto
     public string PictureUrl { get; set; } = string.Empty;
 
     [Required]
-    public string Type { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
 
     [Required]
-    public string Brand { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
 
     [Range(1, int.MaxValue, ErrorMessage = "Quantity in stock must be at least 1")]
     public int QuantityInStock { get; set; }
