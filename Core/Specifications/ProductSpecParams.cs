dc2c49d87faeb526447e2004a8cdbbcbbc4abebb
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-06 13:49:18 +0100

    Clean code

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -11,7 +11,6 @@ public class ProductSpecParams
         get => _pageSize;
         set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
     }
-    
 
     private List<string> _genre = [];
     public List<string> Genre
@@ -43,6 +42,5 @@ public class ProductSpecParams
         get => _search ?? "";
         set => _search = value.ToLower();
     }
-    
 
 }
