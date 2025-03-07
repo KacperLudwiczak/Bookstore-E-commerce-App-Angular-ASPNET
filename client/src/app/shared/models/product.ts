0c8ccdbce08b61ce9508a3d75cc77672fd8ee52d
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-07 14:26:03 +0100

    Create Product model

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    genre: string;
+    publisher: string;
+    quantityInStock: number;
+}
