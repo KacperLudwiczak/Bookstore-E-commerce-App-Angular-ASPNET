2cbf5440d438f2ffb51ee7862c14a9e9b628598b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-12 13:45:39 +0100

    Update provideHttpClient

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+ import { errorInterceptor } from './core/interceptors/error.interceptor';
+ import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 import { routes } from './app.routes';
 
@@ -10,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
   ]
 };
