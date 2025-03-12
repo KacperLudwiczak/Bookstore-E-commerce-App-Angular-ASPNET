48e2ff9c99a870141fae1f14213b64039ae618cb
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-12 14:11:43 +0100

    Add loader

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { BusyService } from './core/services/busy.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Bookstore';
+  busyService = inject(BusyService);
 }
