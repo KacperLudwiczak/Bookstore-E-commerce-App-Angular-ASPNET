3388c23b767718227ccd63f8a8dbe75da8653669
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-13 15:24:06 +0100

    Change loader to bar and put in to header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { BusyService } from './core/services/busy.service';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MatProgressSpinnerModule],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Bookstore';
-  busyService = inject(BusyService);
 }
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,8 +2,10 @@ import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import {MatIcon} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
+import { BusyService } from '../../core/services/busy.service';
+
 
 @Component({
   selector: 'app-header',
@@ -14,11 +16,12 @@ import { CartService } from '../../core/services/cart.service';
     RouterLink,
     RouterLinkActive,
     RouterLinkActive,
-    MatProgressSpinnerModule
+    MatProgressBarModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   cartService = inject(CartService);
+  busyService = inject(BusyService);
 }
