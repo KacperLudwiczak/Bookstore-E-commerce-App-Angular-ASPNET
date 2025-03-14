e8a3f58167356e28af3f710eaa58797bb5b70289
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-14 11:50:15 +0100

    Fix init problems

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideAppInitializer, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -6,29 +6,25 @@ import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 import { InitService } from './core/services/init.service';
 import { lastValueFrom } from 'rxjs';
+import { routes } from './app.routes';
 
-function initializeApp(initService: InitService) {
-  return () => lastValueFrom(initService.init()).finally(() => {
+function initializeApp() {
+  const initService = inject(InitService);
+  
+  return lastValueFrom(initService.init()).finally(() => {
     const splash = document.getElementById('initial-splash');
     if (splash) {
       splash.remove();
     }
-  })
+  });
 }
-
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeApp,
-      multi: true,
-      deps: [InitService]
-    }
-  ]
+    provideAppInitializer(initializeApp),
+  ],
 };
