a215bf8fa786cd19cf77e86f6e090c1abae48715
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-19 14:33:11 +0100

    Add Authorization

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
@@ -19,23 +20,26 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
 builder.Services.AddSingleton<IConnectionMultiplexer>(config => 
- {
-     var connString = builder.Configuration.GetConnectionString("Redis") 
-         ?? throw new Exception("Cannot get redis connection string");
-     var configuration = ConfigurationOptions.Parse(connString, true);
-     return ConnectionMultiplexer.Connect(configuration);
- });
- builder.Services.AddSingleton<ICartService, CartService>();
+{
+    var connString = builder.Configuration.GetConnectionString("Redis") 
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>().AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login
 
 try
 {
