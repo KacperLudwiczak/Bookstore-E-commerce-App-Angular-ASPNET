af1b014159e56dbee9e7a07899a0db28be3545ae
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-20 11:39:17 +0100

    Add account service to header

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import {MatIcon} from '@angular/material/icon';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
 import { BusyService } from '../../core/services/busy.service';
+import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 
 
 @Component({
@@ -16,7 +18,9 @@ import { BusyService } from '../../core/services/busy.service';
     RouterLink,
     RouterLinkActive,
     RouterLinkActive,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatMenuTrigger,
+    MatMenu
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -24,4 +28,15 @@ import { BusyService } from '../../core/services/busy.service';
 export class HeaderComponent {
   cartService = inject(CartService);
   busyService = inject(BusyService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout() {
+    this.accountService.logout().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      }
+    })
+  }
 }
