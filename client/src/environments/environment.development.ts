39b0db7deda74b8a6a44c87ec712fe8d8c4c30cf
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-26 11:45:38 +0100

    Add hub to environment

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5001/api/',
+    hubUrl: 'http://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51R53p9CcxDKxzjSQaZzV1VPoI4CPv88B9s0usEdG4yKpKl5DtHioEr1C66CFtbCEPqm51tuhpDtcHBnuEuLll5cZ00diHMyx0G'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51R53p9CcxDKxzjSQaZzV1VPoI4CPv88B9s0usEdG4yKpKl5DtHioEr1C66CFtbCEPqm51tuhpDtcHBnuEuLll5cZ00diHMyx0G'
 };
