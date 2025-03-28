0ff100460df861f86494094d58e71345487fa9a7
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-28 11:12:43 +0100

    Create IsAdminDirective

--- client/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ client/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
 selector: 'app-confirmation-dialog',
 standalone: true,
-imports: [MatButton],
+imports: [],
 templateUrl: './confirmation-dialog.component.html',
 styleUrl: './confirmation-dialog.component.scss'
 })
