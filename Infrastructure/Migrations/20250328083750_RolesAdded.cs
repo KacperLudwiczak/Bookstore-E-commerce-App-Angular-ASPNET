44f0bc6538a0eb77288b50e1858661c6a4670172
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-03-28 09:38:16 +0100

    Add migrations

---
+++ Infrastructure/Migrations/20250328083750_RolesAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b63c70e4-cf90-44fa-8ec2-03a0722a0b1b", null, "Admin", "ADMIN" },
+                    { "c6bbffb7-bc0a-40c8-85d8-de4a90e5431e", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b63c70e4-cf90-44fa-8ec2-03a0722a0b1b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c6bbffb7-bc0a-40c8-85d8-de4a90e5431e");
+        }
+    }
+}
